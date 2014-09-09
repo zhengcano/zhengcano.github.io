2f607fd857fc0d7fe7cd66ce26679676abc8a13f
zhengcano <zhengcano@gmail.com>
2014-09-09 15:30:51 -0700

    September 9 - Added about default background, fade functionality on contact area, adjusted font colors and opacity

--- js/scroll.js
+++ js/scroll.js
@@ -13,30 +13,70 @@ $(document).ready(function(){
         });
     });
     
+    //Bind about menu to scroll
     $(document).bind('scroll', function(){
         var link = $('.about-nav li');
         var scroll = $window.scrollTop();
        $window.scroll(function(){
             if( scroll >= 700 && scroll <= 1020 ) {
-            link.css('opacity', 1/(320/(scroll - 700) ) );
+            link.css('opacity', 0.8/(320/(scroll - 700) ) );
         } else if (scroll < 700) {
             link.css('opacity', 0);
         } else {
-            link.css('opacity', 1);
+            link.css('opacity', 0.8);
         }
        });
     });
+   
+     //Bind contact info to scroll
+    $(document).bind('scroll', function () {
+        var link = $('#contact h2');
+        var scroll = $window.scrollTop();
+        
+        $window.scroll(function(){
+            
+            if( scroll >= 1020) {
+
+            link.css('opacity', 0.9/(800/(scroll - 881) ) );
+
+            } else {
+
+            link.css('opacity', 0);
+
+            }
+        });
+    });
     
+    $(document).bind('scroll', function () {
+        var link = $('#contact-content');
+        var scroll = $window.scrollTop();
+        
+        $window.scroll(function(){
+            
+            if( scroll >= 1020) {
+
+            link.css('opacity', 0.9/(800/(scroll - 1020) ) );
+
+            } else {
+
+            link.css('opacity', 0);
+
+            }
+        });
+    });
+
+    
+        
     $(document).bind('scroll', function(){
         var link = $('article div');
         var scroll = $window.scrollTop();
        $window.scroll(function(){
             if( scroll >= 200 && scroll <= 700 ) {
-            link.css('opacity', 1/(scroll/200 ) );
+            link.css('opacity', 0.9/(scroll/200 ) );
         } else if (scroll > 700) {
             link.css('opacity', 0);
         } else {
-            link.css('opacity', 1);
+            link.css('opacity', 0.9);
         }
        });
     });
