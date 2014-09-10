125271133149fc20096b7f785b7dba71c86e67da
Robert Zheng <zhengcano@gmail.com>
2014-09-09 20:09:49 -0700

    September 9 - added contact screenshots, adjusted about links to change font-size on hover instead of underline

--- js/links.js
+++ js/links.js
@@ -1,24 +1,28 @@
 $(function () {
-    
+    $('.contact-thumb').hide();
     $('.message').hide();
     
-    function showContact (hover, show) {
+    function showContact (hover, show, img) {
         
         var $hover = $(hover);
         var $show = $(show);
+        var $img = $(img);
         
         $hover.mouseover(function () {
-            $show.show();
+            $show.fadeIn(500);
+            $img.fadeIn(500);
             $hover.mouseleave(function (){
                 $show.hide();
+                $img.hide();
             });
         });
     }
     
-    showContact('#icon1', '#msg1');
-    showContact('#icon2', '#msg2');
-    showContact('#icon3', '#msg3');
-    showContact('#icon4', '#msg4');
+    showContact('#icon1', '#msg1', '#ct1');
+    showContact('#icon2', '#msg2', '#ct2');
+    showContact('#icon3', '#msg3', '#ct3');
+    showContact('#icon4', '#msg4', '#ct4');
+
     
     function changeAbout(link, show, image, color) {
         var $link = $(link);
@@ -26,14 +30,26 @@ $(function () {
         
         $link.click(function(){
             $('#about').css('background-image', "url('" + image + "')");
-            $('.about-nav li').css('color', color)
-            
+            $('.about-nav li').css('color', color);         
 
         });
     }
     
-    changeAbout('#about1', 'show', 'img/background-test.jpg', '#acacae');
+    changeAbout('#about1', 'show', 'img/background-test.jpg', '#e1e1e4');
     changeAbout('#about2', 'show', 'img/dpwedding-bg.jpg', 'black');
     changeAbout('#about3', 'show', 'img/swagwise-bg.jpg', 'black');
     
+    
 });
+
+//Preload Images
+
+$.fn.preload = function() {
+    this.each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+
+// Usage:
+
+$(['img/background-test.jpg','img/dpwedding-bg.jpg','img/swagwise-bg.jpg']).preload();
